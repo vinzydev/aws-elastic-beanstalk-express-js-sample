2a4d864c1dd7e30d09a5de2854b96a69eacb1034
vinzydev <vinzydev@gmail.com>
2024-07-27 12:40:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Idu modalane hadu!'));
+app.get('/', (req, res) => res.send('ee kannalli innu enen nodbeko, alda alda rani, nammavva rani!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
